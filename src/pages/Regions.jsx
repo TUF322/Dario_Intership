6606b22707c408ef68bc31729a789f2ada5997be
José Dario Gutiérrez <136588547+TUF322@users.noreply.github.com>
2024-05-22 15:10:27 +0100

    timeline-implemented

--- src/pages/Regions.jsx
+++ src/pages/Regions.jsx
@@ -2,6 +2,7 @@
 
 import WaveSurfer from 'wavesurfer.js';
 import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js';
+import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.esm.js'
 
 // Utility functions
 const random = (min, max) => Math.random() * (min - max) + min;
@@ -13,6 +14,7 @@ const initializeWaveformWithRegions = (audioUrl, container, loop) => {
     waveColor: 'rgb(75, 75, 200)',
     progressColor: 'rgb(100, 0, 100)',
     url: audioUrl,
+    plugins: [TimelinePlugin.create()],
   });
 
   const wsRegions = ws.registerPlugin(RegionsPlugin.create());
